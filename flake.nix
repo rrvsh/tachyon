7ae3d0dab1d04d368ce57867160e78ff4b87532e
Mohammad Rafiq <rafiq@rrv.sh>
2026-07-27 20:10:01 +0800

    nix: init flake with nixpkgs, flake-parts, import-tree

---
+++ flake.nix
@@ -0,0 +1,9 @@
+{
+  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } (inputs.import-tree ./nix);
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+    import-tree.url = "github:vic/import-tree";
+  };
+}
